b4ee2cd634cf52d758f44b23c5f8297055942e33
ttionya <git@ttionya.com>
2017-06-19 18:02:29 +0800

    Ver 1.4.0：不完整适配使用了 CSS-Modules 的百度云

--- BaiduYun-PCS-Download.user.js
+++ BaiduYun-PCS-Download.user.js
@@ -11,7 +11,7 @@
 // @downloadURL https://raw.githubusercontent.com/ttionya/BaiduYun-PCS-Download/master/BaiduYun-PCS-Download.user.js
 // @run-at      document-end
 // @grant       none
-// @version     1.3.0
+// @version     1.4.0
 // ==/UserScript==
 
 
@@ -19,24 +19,64 @@
     "use strict";
 
     var d = document,
-        toolsLastButton = d.querySelector('.tools > span:last-child'),
+        tools = { // 工具
+            /*
+             * 祖先元素
+             * elem: element
+             * className: className support only
+             */
+            parents: function (elem, className) {
+                var matched = [];
+
+                while ((elem = elem.parentNode) && elem.nodeType !== 9) {
+                    if (elem.nodeType === 1 && elem.classList.contains(className)) {
+                        matched.push(elem);
+                    }
+                }
+
+                return matched;
+            }
+        },
         dlLink = 'http://pcs.baidu.com/rest/2.0/pcs/file?method=download&app_id=266719&path=',
-        dlButton, iframeDiv;
+        time = 10,
+        toolsLastButton, dlButton, iframeDiv, interval;
 
+    // 循环判断是否取到元素，只判断 10 次
+    // 因为莫名取不到相应元素，只好延迟判断
+    interval = setInterval(function () {
+        time--;
+        toolsLastButton = tools.parents(d.querySelector('[title=上传]'), 'g-dropdown-button')[0]; // '.tools > span:last-child'
 
-    // 添加伪元素、按钮、容器和监听
-    addStyle();
-    dlButton = addButton();
-    iframeDiv = addDiv();
-    dlButton.addEventListener('click', getdlUri);
+        // 取到了
+        if (toolsLastButton) {
+            toolsLastButton = toolsLastButton.parentNode.lastChild;
 
+            clearInterval(interval); // 取消
 
-    // 第一次加载时判断是否显示
-    checkUri(location.href);
-    // hash 变化时判断是否显示
-    window.addEventListener('hashchange', function (e) {
-        checkUri(e.newURL);
-    });
+            init(); // 初始化
+        }
+        else if (!time) {
+            console.warn('百度网盘黑科技获取 DOM 失败');
+
+            clearInterval(interval); // 取消，没有然后了
+        }
+    }, 500);
+
+    // 初始化
+    function init () {
+        // 添加伪元素、按钮、容器和监听
+        addStyle();
+        dlButton = addButton();
+        iframeDiv = addDiv();
+        dlButton.addEventListener('click', getdlUri);
+
+        // 第一次加载时判断是否显示
+        checkUri(location.href);
+        // hash 变化时判断是否显示
+        window.addEventListener('hashchange', function (e) {
+            checkUri(e.newURL);
+        });
+    }
 
 
     // 获得下载地址
@@ -51,12 +91,12 @@
         if (hash.indexOf('vmode=grid') !== -1) {
 
             // 网格模式
-            activeItems = d.querySelectorAll('.grid-view-item.item-active');
+            activeItems = d.querySelectorAll('.cEefyz.duqD6l'); // '.grid-view-item.item-active'
         }
         else {
 
             // 列表模式
-            activeItems = d.querySelectorAll('.list-view-item.item-active');
+            activeItems = d.querySelectorAll('.AuPKyz.duqD6l'); // '.list-view-item.item-active'
         }
 
         activeItemsLen = activeItems.length;
@@ -71,7 +111,7 @@
         }
         else {
             for (; activeItemsLen--;) {
-                filename = activeItems[activeItemsLen].querySelector(".file-name .filename").innerText;
+                filename = activeItems[activeItemsLen].querySelector(".file-name .yezXdy").innerText; // '.file-name .filename'
 
                 var iframe = d.createElement('iframe');
                 iframe.style.display = 'none';
@@ -88,7 +128,7 @@
 
         a.innerHTML = '<span class="g-button-right"><em class="icon icon-black" title="黑科技下载"></em><span class="text" style="width: auto;">黑科技下载</span></span>';
         a.id = 'PCS-dl';
-        a.classList.add('g-button', 'hidden');
+        a.classList.add('g-button', 'PCS-hidden');
 
         toolsLastButton.parentNode.insertBefore(a, toolsLastButton);
 
@@ -109,7 +149,7 @@
     function addStyle() {
         var style = d.createElement('style');
 
-        style.innerText = '.icon-black::before{content:"\\9ed1";}.hidden{display:none;}';
+        style.innerText = '.icon-black::before{content:"\\9ed1";}.PCS-hidden{display:none;}';
         d.head.appendChild(style);
     }
 
@@ -119,12 +159,12 @@
 
             // 不是“全部文件”页面
             // 是搜索页面
-            dlButton.classList.add('hidden');
+            dlButton.classList.add('PCS-hidden');
         }
         else {
             url.indexOf('category/type=') === -1 ?
-                dlButton.classList.remove('hidden') :
-                dlButton.classList.add('hidden');
+                dlButton.classList.remove('PCS-hidden') :
+                dlButton.classList.add('PCS-hidden');
         }
     }
 
@@ -133,7 +173,7 @@
         for (; len--;) {
 
             // 确认是否是文件夹
-            if (activeItems[len].querySelector('.fileicon').classList.value.indexOf('dir-') !== -1) {
+            if (activeItems[len].querySelector('.mtglG7l').classList.value.indexOf('dir-') !== -1) { // '.fileicon'
                 return false;
             }
         }
